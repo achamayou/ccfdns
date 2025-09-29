cb9d9b60b4ce31f8bf910bea39ca693be5fe24ed
Amaury Chamayou <amaury@xargs.fr>
2025-09-29 14:00:21 +0000

    wip

--- tests/test_adns_attestation_verification.py
+++ tests/test_adns_attestation_verification.py
@@ -3,13 +3,32 @@
 
 import sys
 import cbor2
+import cwt
+import json
+import base64
 import snp_pytools
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+
+def string_to_cert(pem_string):
+    return x509.load_pem_x509_certificate(pem_string.encode(), default_backend())
 
 def verify_attestation(attestation_cbor):
     # Extract certs from cbor and pass them to avoid AMD access
-    report, certs, report_data_hex = snp_pytools.verify_attestation_bytes(attestation_cbor["att"], processor_model="milan")
+    cert_chain = json.loads(base64.b64decode(attestation_cbor["eds"]))
+    vceck = cert_chain['vcekCert']
+    chain = cert_chain['certificateChain'].split("-----END CERTIFICATE-----\n")
+    certificates = {
+        'ark': string_to_cert(chain[1] + "-----END CERTIFICATE-----\n"),
+        'ask': string_to_cert(chain[0] + "-----END CERTIFICATE-----\n"),
+        'vcek': string_to_cert(vceck),
+    }
+    report, certs, report_data_hex = snp_pytools.verify_attestation_bytes(attestation_cbor["att"], processor_model="milan", certificates=certificates, certificates_path="ca")
+    #report, certs, report_data_hex = snp_pytools.verify_attestation_bytes(attestation_cbor["att"], processor_model="milan")
     print(report.host_data)
     print(report_data_hex)
+    print(report.measurement)
+    # cose_ctx = cwt.COSE.new()
     # UVM descriptor check
     # Return platform & UVM descriptor in a format Rego can use
 
